413d432e8b1a4318fd76d07e5e997f667e94b579
Freddy Martin <35710805+Hubcat123@users.noreply.github.com>
2026-01-20 00:29:28 -0800

    Added support for remaining vanilla patterns into assembler. New patterns are not usable in code, however

--- src/assembler.hpp
+++ src/assembler.hpp
@@ -9,15 +9,24 @@ public:
     std::string assemble();
 private:
     const std::string m_pattern_strings[PatternType::num_patterns] = {
-        "Additive Distillation", "Alidade's Purification", "Archer's Distillation", "Architect's Distillation", "Augur's Exaltation", "Augur's Purification", "Bookkeeper's Gambit", "Break Block",
-        "Charon's Gambit", "Compass' Purification", "Compass' Purification II", "Conjunction Distillation", "Conjure Light", "Consideration", "Create Water", "Dioscuri Gambit",
-        "Disjunction Distillation", "Division Distillation", "Equality Distillation", "False Reflection", "Fisherman's Gambit", "Fisherman's Gambit II", "Flock's Disintegration", "Flock's Gambit",
-        "Flock's Reflection", "Gemini Decomposition", "Hermes' Gambit", "Huginn's Gambit", "Inequality Distillation", "Integration Distillation", "{", "Iris' Gambit", "Jester's Gambit",
-        "Length Purification", "Locator's Distillation", "Maximus Distillation", "Maximus Distillation II", "Mind's Reflection", "Minimus Distillation", "Minimus Distillation II",
-        "Modulus Distillation", "Multiplicative Distillation", "Muninn's Reflection", "Negation Purification", "Nullary Reflection", "Numerical Reflection", "Power Distillation",
-        "Prospector's Gambit", "}", "Reveal", "Rotation Gambit", "Rotation Gambit II", "Single's Purification", "Selection Distillation", "Subtractive Distillation", "Surgeon's Exaltation",
-        "Thoth's Gambit", "True Reflection", "Vacant Reflection", "Vector Exaltation", "Vector Reflection Zero", "Vector Reflection +X", "Vector Reflection -X", "Vector Reflection +Y",
-        "Vector Reflection -Y", "Vector Reflection +Z", "Vector Reflection -Z"
+        "Akasha's Distillation", "Akasha's Gambit", "Additive Distillation", "Alidade's Purification", "Anchorite's Flight", "Archer's Distillation",
+        "Architect's Distillation", "Arc's Reflection", "Augur's Exaltation", "Augur's Purification", "Auditor's Purification", "Auditor's Reflection",
+        "Assessor's Purification", "Assessor's Reflection", "Aviator's Purification", "Black Sun's Nadir", "Black Sun's Zenith", "Blue Sun's Nadir",
+        "Blue Sun's Zenith", "Bookkeeper's Gambit", "Break Block", "Charon's Gambit", "Chronicler's Gambit", "Chronicler's Purification", "Circle's Reflection",
+        "Compass' Purification", "Compass' Purification II", "Conjunction Distillation", "Conjure Light", "Consideration", "Cosine Purification", "Create Water",
+        "Derivation Decomposition", "Dioscuri Gambit", "Disjunction Distillation", "Division Distillation", "Dispel Rain", "Euler's Reflection",
+        "Equality Distillation", "Evanition", "Explosion", "False Reflection", "Fireball", "Floor Purification", "Fisherman's Gambit", "Fisherman's Gambit II",
+        "Flock's Disintegration", "Flock's Gambit", "Flock's Reflection", "Gemini Decomposition", "Gemini Gambit", "Greater Fold Reflection", "Hermes' Gambit",
+        "Huginn's Gambit", "Inequality Distillation", "Integration Distillation", "Inverse Cosine Purification", "Inverse Sine Purification",
+        "Inverse Tangent Distillation", "Introspection", "Iris' Gambit", "Jester's Gambit", "Length Purification", "Locator's Distillation", "Lodestone Reflection",
+        "Logarithmic Distillation", "Make Note", "Maximus Distillation", "Maximus Distillation II", "Mind's Reflection", "Minimus Distillation",
+        "Minimus Distillation II", "Modulus Distillation", "Multiplicative Distillation", "Muninn's Reflection", "Negation Purification", "Nullary Reflection",
+        "Numerical Reflection", "Pace Purification", "Power Distillation", "Prospector's Gambit", "Retrospection", "Reveal", "Rotation Gambit", "Rotation Gambit II",
+        "Selection Distillation", "Selection Exaltation", "Speaker's Decomposition", "Speaker's Distillation", "Single's Purification", "Stadiometer's Purification",
+        "Subtractive Distillation", "Sine Purification", "Swindler's Gambit", "Summon Lightning", "Summon Rain", "Surgeon's Exaltation", "Thanatos' Reflection",
+        "Thoth's Gambit", "True Reflection", "Vacant Reflection", "Vector Disintegration", "Vector Exaltation", "Vector Reflection -X", "Vector Reflection -Y",
+        "Vector Reflection -Z", "Vector Reflection +X", "Vector Reflection +Y", "Vector Reflection +Z", "Vector Reflection Zero", "Wayfarer's Flight",
+        "Waystone Reflection", "White Sun's Nadir", "White Sun's Zenith"
     };
 
     std::vector<Pattern> m_patterns;
